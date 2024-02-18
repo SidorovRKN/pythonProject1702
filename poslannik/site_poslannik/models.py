1c762447c05d3d7f2a9c22d25bf7fb1ff7f21c6b
kolya <kolya.sidorov00@gmail.com>
2024-02-18 21:41:01 +0300

    add model

--- poslannik/site_poslannik/models.py
+++ poslannik/site_poslannik/models.py
@@ -4,5 +4,6 @@ from django.db import models
 # Create your models here.
 class Parts(models.Model):
     name = models.CharField(max_length=200)
-    code = models.CharField(max_length=200, unique=True, primary_key=True)
+    code = models.IntegerField(unique=True, primary_key=True)
+    category = models.IntegerField(verbose_name='cat', default=1)
     descr = models.TextField(blank=True)
