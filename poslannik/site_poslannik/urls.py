13b211f684e03ce05aae5308b563766bcf188cf0
kolya <kolya.sidorov00@gmail.com>
2024-03-23 00:25:33 +0300

    class based views

--- poslannik/site_poslannik/urls.py
+++ poslannik/site_poslannik/urls.py
@@ -2,12 +2,13 @@ from django.urls import path, include
 from . import views
 from .converters import *
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('about/', views.about, name='about'),
-    path('part/<slug:part_slug>/', views.show_part, name='part'),
-    path('contact/', views.contact, name='contact'),
-    path('login/', views.login, name='login'),
-    path('archive/<yyyy:year>/', views.archive, name='arhive'),
-    path('category/<slug:cat_slug>/', views.cats, name='category'),
-    path('searchpart/', views.search, name='search'),
+    path('', views.IndexView.as_view(), name='home'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('part/<slug:part_slug>/', views.ShowPartView.as_view(), name='part'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('archive/<yyyy:year>/', views.ArchiveView.as_view(), name='archive'),  # Использование int вместо yyyy
+    path('category/<slug:cat_slug>/', views.IndexView.as_view(), name='category'),
+    path('searchpart/', views.SearchView.as_view(), name='search'),
+    path('addpart/', views.AddPartView.as_view(), name='addpart')
 ]
--- poslannik/site_poslannik/views.py
+++ poslannik/site_poslannik/views.py
@@ -5,6 +5,8 @@ from django.template.defaultfilters import slugify
 from django.urls import reverse
 from django.template.loader import render_to_string
 from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
+from django.views import View
+from django.views.generic import ListView, DetailView
 from site_poslannik.models import Parts, Category
 
 # Create your views here.
@@ -16,83 +18,96 @@ menu = [
 ]
 
 
-def index(request):
-    data = {
-        'title': 'Автозапчасти в Борисове',
-        'menu': menu,
-        'parts': Parts.objects.all().order_by('name')[:20],
-        'cats': Category.objects.all(),
-        'default_descr': 'Описание товара № XXXXX'
+class IndexView(ListView):
+    template_name = 'site_poslannik/index.html'
+    context_object_name = 'parts'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["menu"] = menu
+        context["cats"] = Category.objects.all()
+        context["title"] = "Автозапчасти в Борисове"
 
-    }
-    return render(request, 'site_poslannik/index.html', context=data)
+        # костыль пока нет описаний
+        context["default_descr"] = "Описание товара № ХХХХ"
 
+        cat_slug = self.kwargs.get('cat_slug')
+        if cat_slug:
+            context["cat_selected"] = cat_slug
+        part_slug = self.kwargs.get('part_slug')
+        if part_slug:
+            context["part"] = Parts.objects.get(slug=part_slug)
+        return context
 
-def show_part(request, part_slug):
-    part = get_object_or_404(Parts, slug=part_slug)
-
-    data = {
-        'title': f'{part.name}',
-        'name': part.name,
-        'menu': menu,
-        'part': part,
-        'cats': Category.objects.all(),
-        'default_descr':f'Описание товара № {part.pk}'
-    }
-
-    return render(request, 'site_poslannik/part.html', context=data)
-
-
-def about(request):
-    return render(request, 'site_poslannik/about.html', {'title': 'О нас', 'menu': menu})
-
-
-def cats(request, cat_slug):
-    selected = Category.objects.get(slug=cat_slug)
-    data = {
-        'title': f'{selected.name}',
-        'parts': selected.parts_set.all()[:20],
-        'menu': menu,
-        'cat_selected': cat_slug,
-        'cats': Category.objects.all()
-    }
-    return render(request, 'site_poslannik/index.html', context=data)
+    def get_queryset(self):
+        cat_slug = self.kwargs.get('cat_slug')
+        if cat_slug:
+            cat_selected = Category.objects.get(slug=cat_slug)
+            return Parts.objects.filter(category=cat_selected).order_by('name')[:20]
+        else:
+            return Parts.objects.all().order_by('name')[:20]
 
 
-def search(request):
-    query_string = request.GET.get('q')
-    parts = Parts.objects.annotate(search=SearchVector('name', 'descr', 'category')).filter(search=query_string)
-    data = {
-        'title': 'Автозапчасти в Борисове',
-        'menu': menu,
-        'parts': parts,
-        'cats': Category.objects.all()
-    }
-    return render(request, 'site_poslannik/search.html', data)
+
+class ShowPartView(DetailView):
+    model = Parts
+    template_name = 'site_poslannik/part.html'
+    context_object_name = 'part'
+    slug_field = 'slug'  # Поле в модели, используемое как slug
+    slug_url_kwarg = 'part_slug'  # Имя параметра в URLconf, которое содержит slug
+
+class AddPartView(View):
+    def get(self, request):
+        data = {
+            'title': f'Добавить запчасть',
+            'menu': menu,
+            'cats': Category.objects.all(),
+        }
+        return render(request, 'site_poslannik/addpart.html', context=data)
 
 
-def archive(request, year):
-    if year > 2023:
-        uri = reverse('cats_slug', args=('bebracat2000',))
-        return redirect(uri)
-    return HttpResponse(f'In {year} year... ')
+class AboutView(View):
+    def get(self, request):
+        return render(request, 'site_poslannik/about.html', {'title': 'О нас', 'menu': menu})
 
 
-def login(request):
-    return HttpResponse('<h1>Login</h1>')
+class SearchView(View):
+    def get(self, request):
+        query_string = request.GET.get('q')
+        parts = Parts.objects.annotate(search=SearchVector('name', 'descr', 'category')).filter(search=query_string)
+        data = {
+            'title': 'Автозапчасти в Борисове',
+            'menu': menu,
+            'parts': parts,
+            'cats': Category.objects.all()
+        }
+        return render(request, 'site_poslannik/search.html', data)
 
 
-def contact(request):
-    data = {
-        'title': 'Контакты для заказа',
-        'num': '76-36-02',
-        'menu': menu,
-        'email': 'some_email@gmail.com',
-        'cats': Category.objects.all()
+class ArchiveView(View):
+    def get(self, request, year):
+        if year > 2023:
+            uri = reverse('cats_slug', args=('bebracat2000',))
+            return redirect(uri)
+        return HttpResponse(f'In {year} year... ')
 
-    }
-    return render(request, 'site_poslannik/contact.html', context=data)
+
+class LoginView(View):
+    def get(self, request):
+        return HttpResponse('<h1>Login</h1>')
+
+
+class ContactView(View):
+    def get(self, request):
+        data = {
+            'title': 'Контакты для заказа',
+            'num': '76-36-02',
+            'menu': menu,
+            'email': 'some_email@gmail.com',
+            'cats': Category.objects.all()
+        }
+        return render(request, 'site_poslannik/contact.html', context=data)
 
 
 def page_not_found(request, exception):
-    return redirect(index)
+    return redirect(IndexView.as_view)
